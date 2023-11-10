981fe7a940d23cce76df2deaa5fc0f612b9837b2
Nozima29 <nozima.9813@gmail.com>
2023-11-10 13:00:49 +0900

    adding argparse

---
+++ attack_runner.py
@@ -0,0 +1,28 @@
+import tensorflow as tf
+
+from _utils.helper import get_attack_inp, is_valid
+from attacks.mia.custom import run_custom_attacks
+from attacks.mia.advanced import train_shadows, run_advanced_attack
+from target.tf_target import load_cifar10, train
+
+
+def runner(args):
+    mpath = args.model_path
+    attack = args.attack
+
+    if not is_valid(mpath):
+        train(mpath)
+
+    model = tf.keras.models.load_model(mpath, compile=False)
+    tdata = load_cifar10()
+
+    if attack == 'custom':
+        attack_input = get_attack_inp(model, tdata)
+        run_custom_attacks(attack_input)
+
+    elif attack == 'advanced':
+        adata = train_shadows(model, tdata)
+        run_advanced_attack(adata)
+
+    else:
+        raise NotImplementedError('The other type of attacks not implemented!')
--- main.py
+++ main.py
@@ -1,29 +1,13 @@
-import warnings
-import tensorflow as tf
-from _utils.helper import get_attack_inp, is_valid
-from attacks.mia.custom import run_custom_attacks
-from attacks.mia.advanced import train_shadows, run_advanced_attack
-from target.tf_target import load_cifar10, train
-import sys
-import os
-
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
-warnings.filterwarnings("ignore")
+import argparse
+from attack_runner import runner
 
 if __name__ == '__main__':
-    arg = sys.argv[-1]  # option 1: custom attack;  option 2: advanced attack
-
     mpath = 'target/weights/cifar10_densenet.h5'
-
-    if not is_valid(mpath):
-        train(mpath)
-
-    model = tf.keras.models.load_model(mpath, compile=False)
-    tdata = load_cifar10()
-
-    if arg == 1:
-        attack_input = get_attack_inp(model, tdata)
-        run_custom_attacks(attack_input)
-    elif arg == 2:
-        adata = train_shadows(model, tdata)
-        run_advanced_attack(adata)
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('--model_path', default=mpath,
+                        help='an absolute path where pretrained model is saved')
+    parser.add_argument('--attack', default='custom',
+                        help='An attack type: "custom" | "advanced" ')
+    args = parser.parse_args()
+    print(args)
+    runner(args)
